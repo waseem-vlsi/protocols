0fe6555c0bfd784a9a3245f88217d9776096bea5
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-21 15:07:38 +0530

    Update code.sv

--- AHB/slave/read_fsm/code.sv
+++ AHB/slave/read_fsm/code.sv
@@ -9,3 +9,65 @@ module slave_read_fsm(
     output logic HREADY
 
 );
+
+
+
+    always@(posedge clock or posedge reset) begin 
+
+        if(reset) 
+            present_state <= IDLE;
+        else 
+            present_state <= next_state;
+    end 
+
+always_ff @(posedge clock or posedge reset) begin 
+    if(reset)
+        addr_reg <= 32'd0;
+    else if(present_state == IDLE &&
+            HTRANS == NONSEQ &&
+            HWRITE == 0)
+        addr_reg <= HADDR;
+end
+    
+always_ff @(posedge clock or posedge reset) begin 
+    if(reset) 
+        data_reg <= 32'd0;
+
+    else if(present_state == FETCH_DATA)
+        data_reg <= mem[addr_reg];
+end
+
+
+
+    always@(*) begin 
+        present_state = next_state;
+        case(present_state)
+        IDLE: 
+
+
+        endcase 
+    
+
+    end 
+
+
+    always@(*) begin 
+        HREADY = 0;
+        HRDATA = 0;
+
+        case(present_state)
+            IDLE: begin 
+            end 
+
+            ADDRESS_DECODE: begin 
+            end
+
+            FETCH_DATA: begin 
+            end 
+            TRANSFER_DATA: begin 
+                HREADY = 1;
+                HRDATA = data_reg;
+            end
+
+        endcase
+    end
