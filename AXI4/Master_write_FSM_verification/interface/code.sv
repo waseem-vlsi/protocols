4396c2571f4904901a7e9936e56d8f29a1c648be
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-08-12 21:55:06 +0530

    Update code.sv

--- AXI4/Master_write_FSM_verification/interface/code.sv
+++ AXI4/Master_write_FSM_verification/interface/code.sv
@@ -1,26 +1,26 @@
 interface master_write_FSM_if;
 
- logic clock,
- logic reset_n,
- logic write_req,
- logic [31:0]address,
- logic [31:0]data,
- logic [7:0]awlen,
- logic [2:0]awsize,
- logic [1:0]awburst,
- logic AWREADY,
- logic WREADY,
- logic BVALID,
- logic [1:0] BRESP,
- logic [31:0]AWADDR,
- logic AWVALID,
- logic [7:0]AWLEN,
- logic [2:0]AWSIZE,
- logic [1:0]AWBURST,
- logic [31:0]WDATA,
- logic WVALID,
- logic [3:0]WSTRB,
- logic WLAST,
- logic BREADY
+ logic clock;
+ logic reset_n;
+ logic write_req;
+ logic [31:0]address;
+ logic [31:0]data;
+ logic [7:0]awlen;
+ logic [2:0]awsize;
+ logic [1:0]awburst;
+ logic AWREADY;
+ logic WREADY;
+ logic BVALID;
+ logic [1:0] BRESP;
+ logic [31:0]AWADDR;
+ logic AWVALID;
+ logic [7:0]AWLEN;
+ logic [2:0]AWSIZE;
+ logic [1:0]AWBURST;
+ logic [31:0]WDATA;
+ logic WVALID;
+ logic [3:0]WSTRB;
+ logic WLAST;
+ logic BREADY;
   
 endinterface
