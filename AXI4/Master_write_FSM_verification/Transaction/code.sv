ada74abee38db582cfac3495d70bea3b5fd394d5
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-08-12 20:40:16 +0530

    Create code.sv

---
+++ AXI4/Master_write_FSM_verification/Transaction/code.sv
@@ -0,0 +1,14 @@
+class transaction;
+  rand logic [31:0] AWADDR;
+  rand logic [7:0] AWLEN;
+  rand logic [2:0] AWSIZE;
+  rand logic [1:0] AWBURST;
+  rand logic WLAST;
+  rand logic [31:0] AWDATA;
+  rand logic [3:0] WSTRB;
+
+       
+  
+    
+
+endclass 
