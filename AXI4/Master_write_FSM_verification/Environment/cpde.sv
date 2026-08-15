b37bb66e631dcaa9dc7a3972b97e661bcc457d7b
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-08-15 21:09:25 +0530

    Create cpde.sv

---
+++ AXI4/Master_write_FSM_verification/Environment/cpde.sv
@@ -0,0 +1,9 @@
+class environment;
+  mailbox #(transaction_master_write) mbxg2d;
+  mailbox #(transaction_master_write) mbxg2s;
+  mailbox #(transaction_master_write) mbxm2s;
+
+  transaction_master_write tr;
+  
+  
+endclass 
